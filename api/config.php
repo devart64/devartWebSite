2b4ae969b4d28526cc64c2a063b7ac8949756b0d
stephen <stephendupre64@gmail.com>
2017-05-18 13:37:54 +0200

    Commit the front-end of my web site

---
+++ api/config.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', '0QRb5hPJw5AsUTZ3WQ8YJxlpZ');
+    define('CONSUMER_SECRET', 'O8tX8Z19KN6vQoAkaRo69dbRJV5l5ge54JwsknhdlXIA4Mk61T');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '785492712921366528-kG8Ux9m69wNt8oaVrciCJujam3Ovg5G');
+    define('ACCESS_SECRET', 'mdzJNExe7BQPSXUmLGwAGR1ztFIyeTeLRY2UPrLkO1xh6');
